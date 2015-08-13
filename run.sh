bc707e90e1dccf4d8f4879af75a1459306d562e1
Andrew Teixeira <teixeira@broadinstitute.org>
2015-08-13 14:48:16 -0400

    initial commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+export LANG=C
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export TZ=America/New_York
+
+cd /opt/orientdb/bin
+
+exec ./server.sh 2>&1
